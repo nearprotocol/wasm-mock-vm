67ac397ff58daf2edc8d87f0e0fcfc5c858325cd
Willem Wyndham <willem@cs.umd.edu>
2020-03-03 14:13:17 -0500

    add run_wasm function as start of runner

--- pkg/wasm_mock_vm_bg.d.ts
+++ pkg/wasm_mock_vm_bg.d.ts
@@ -73,6 +73,7 @@ export function vm_storage_iter_prefix(a: number, b: number, c: number, d: numbe
 export function vm_storage_iter_range(a: number, b: number, c: number, d: number, e: number, f: number, g: number, h: number, i: number, j: number): void;
 export function vm_storage_iter_next(a: number, b: number, c: number, d: number, e: number, f: number, g: number, h: number): void;
 export function vm_outcome(a: number): number;
+export function run(a: number): void;
 export function __wbindgen_malloc(a: number): number;
 export function __wbindgen_realloc(a: number, b: number, c: number): number;
 export function __wbindgen_free(a: number, b: number): void;
